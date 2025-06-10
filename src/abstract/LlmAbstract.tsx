83b71afbe1fda6527027063e6c3798e59b098ea2
Danil Belous <flexsovoi@yandex.ru>
2025-06-10 08:34:43 +0300

    fix

--- src/abstract/LlmAbstract.tsx
+++ src/abstract/LlmAbstract.tsx
@@ -1,4 +1,9 @@
 import Script from "next/script"
+import AiMathBegin from "./blocks/AiMath/AiMathBegin"
+import LineAlgebra from "./blocks/AiMath/LineAlgebra"
+import MathAnalytic from "./blocks/AiMath/MathAnalytic"
+import HistoryBlock from "./blocks/history/HistoryBlock"
+import MechanismAttention from "./blocks/MechanismAttention/MechanismAttention"
 import Transformers from "./blocks/transformers/Transformers"
 import Title from "./components/Title/Title"
 import { scaleFunction } from "./lib/scale-function"
@@ -16,16 +21,16 @@ const LlmAbstract = () => {
 				className="sc-p-4 font-onest sc-text-4 m-auto mt-10 max-w-[1024px] overflow-hidden"
 				id="abstract"
 			>
-				{/* <Title>История LLM</Title>
+				<Title>История LLM</Title>
 				<HistoryBlock />
 				<Title>Математика нейросетей</Title>
-				<AiMathBegin /> */}
-				{/* <Title>Линейная алгебра</Title>
+				<AiMathBegin />
+				<Title>Линейная алгебра</Title>
 				<LineAlgebra />
 				<Title>Математический анализ</Title>
-				<MathAnalytic /> */}
-				{/* <Title>Механизм внимания</Title>
-				<MechanismAttention /> */}
+				<MathAnalytic />
+				<Title>Механизм внимания</Title>
+				<MechanismAttention />
 				<Title>Трансформеры</Title>
 				<Transformers />
 			</div>
